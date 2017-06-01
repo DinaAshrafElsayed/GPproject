4e7a3b9453b1bfc11691a29ec8a0602cac6ed2e4
DinaAshrafElsayed <dina.ashrafelsayed@gmail.com>
2017-06-01 10:32:03 +0200

    register and login done

--- ws/src/main/java/eg/iti/shareit/ws/registrationWS.java
+++ ws/src/main/java/eg/iti/shareit/ws/registrationWS.java
@@ -1,14 +1,16 @@
 package eg.iti.shareit.ws;
 
+import eg.iti.shareit.common.Exception.ServiceException;
 import eg.iti.shareit.model.dto.GenderDto;
 import eg.iti.shareit.model.dto.UserDto;
+import eg.iti.shareit.model.util.HashingUtil;
+import eg.iti.shareit.model.util.ImageUtil;
 import eg.iti.shareit.service.UserService;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Level;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -36,74 +38,55 @@ public class registrationWS {
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes("multipart/form-data")
-    public Response uploadUserImage(MultipartFormDataInput input) {
-
+    public Response Register(MultipartFormDataInput input) {
         Map<String, List<InputPart>> uploadForm = input.getFormDataMap();
-
         // Get file data to save
         List<InputPart> inputParts = uploadForm.get("attachment");
-
-        for (InputPart inputPart : inputParts) {
-            try {
-
+        String fileName = "";
+        Response response;
+        try {
+            for (InputPart inputPart : inputParts) {
                 MultivaluedMap<String, String> header = inputPart.getHeaders();
-                String fileName = getFileName(header);
-
+                ImageUtil imageUtil = new ImageUtil();
+                fileName = imageUtil.getFileName(header);
                 // convert the uploaded file to inputstream
                 InputStream inputStream = inputPart.getBody(InputStream.class,
                         null);
-
                 byte[] bytes = IOUtils.toByteArray(inputStream);
                 // constructs upload file path and saves it
                 fileName = "/home/user/profile/" + fileName;
-                writeFile(bytes, fileName);
-
-                UserDto userDto = new UserDto();
-                userDto.setUsername(input.getFormDataPart("username", String.class, null));
-                userDto.setEmail(input.getFormDataPart("email", String.class, null));
-                userDto.setPassword(input.getFormDataPart("password", String.class, null));
-                userDto.setImageUrl(fileName);
-                userDto.setPoints(100);
-                GenderDto genderDto = new GenderDto();
-                genderDto.setGender(input.getFormDataPart("gender", String.class, null));
-                userDto.setGender(genderDto);
-
-                //save url to database
-                return Response.status(200).entity("Uploaded file name : " + fileName)
-                        .build();
-            } catch (IOException e) {
-                e.printStackTrace();
+                imageUtil.writeFile(bytes, fileName);
             }
+            UserDto userDto = buildUser(input, fileName);
+            userService.RegisterUser(userDto);
+            response = Response.status(200).entity("user Registered : " + userDto)
+                    .build();
+        } catch (IOException e) {
+            logger.log(Level.SEVERE, "IO exception occurred", e);
+            response = Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("IO exception occurred please try again later").build();
+        } catch (ServiceException e) {
+            logger.log(Level.SEVERE, "service exception occurred", e);
+            response = Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("Service exception occurred please try again later").build();
+        } catch (Exception e) {
+            logger.log(Level.SEVERE, "unexpected error", e);
+            response = Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("Unexpected error has been occurred please try again later").build();
         }
-        return null;
+        return response;
     }
 
-    private String getFileName(MultivaluedMap<String, String> header) {
-
-        String[] contentDisposition = header.getFirst("Content-Disposition").split(";");
-
-        for (String filename : contentDisposition) {
-            if ((filename.trim().startsWith("filename"))) {
-
-                String[] name = filename.split("=");
-
-                String finalFileName = name[1].trim().replaceAll("\"", "");
-                return finalFileName;
-            }
-        }
-        return "unknown";
-    }
-
-    // Utility method
-    private void writeFile(byte[] content, String filename) throws IOException {
-        File file = new File(filename);
-        if (!file.exists()) {
-            System.out.println("not exist > " + file.getAbsolutePath());
-            file.createNewFile();
-        }
-        try (FileOutputStream fop = new FileOutputStream(file)) {
-            fop.write(content);
-            fop.flush();
-        }
+    private UserDto buildUser(MultipartFormDataInput input, String fileName) throws IOException {
+        //remains saving user address needs to fix database
+        UserDto userDto = new UserDto();
+        userDto.setUsername(input.getFormDataPart("username", String.class, null));
+        userDto.setEmail(input.getFormDataPart("email", String.class, null));
+        String password = input.getFormDataPart("password", String.class, null);
+        HashingUtil hashingUtil = new HashingUtil();
+        userDto.setPassword(hashingUtil.getHashedPassword(password));
+        userDto.setImageUrl(fileName);
+        userDto.setPoints(100);
+        GenderDto genderDto = new GenderDto();
+        genderDto.setGender(input.getFormDataPart("gender", String.class, null));
+        userDto.setGender(genderDto);
+        return userDto;
     }
 }
