64500881fa0e46475c1ac405d44619ef73d8c958
DinaAshrafElsayed <dina.ashrafelsayed@gmail.com>
2017-06-20 02:19:10 +0200

    made items not transient in user Entity

--- business/src/main/java/eg/iti/shareit/model/entity/UserEntity.java
+++ business/src/main/java/eg/iti/shareit/model/entity/UserEntity.java
@@ -19,6 +19,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.CascadeType;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
@@ -53,7 +55,7 @@ public class UserEntity implements Serializable, GenericEntity {
     private List<NotificationEntity> notificationFromUserList;
     @javax.persistence.Transient
     private List<NotificationEntity> notificationToUserList;
-    @javax.persistence.Transient
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "userFrom", fetch = FetchType.LAZY)
     private List<ItemEntity> items;
 
     private static final long serialVersionUID = 1L;
